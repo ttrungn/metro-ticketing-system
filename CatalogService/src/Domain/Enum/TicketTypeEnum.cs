2380c0f9a8db399d649b6071d0c5cd752ef0dcae
Bui Gia Huy <buigiahuy403@gmail.com>
2025-06-15 22:18:44 +0700

    fix ticket enum

--- CatalogService/src/Domain/Enum/TicketTypeEnum.cs
+++ CatalogService/src/Domain/Enum/TicketTypeEnum.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace CatalogService.Domain.Enum;
 public enum TicketTypeEnum
 {
-    SingleUseType =1 ,
+    SingleUseType = 1,
     MultiUseType = 2,
-    StudentType =3,
+    StudentType = 3,
 }
