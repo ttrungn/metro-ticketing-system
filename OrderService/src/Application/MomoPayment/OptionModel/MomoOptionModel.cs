c621ad632e91cfca795a85d5da2a6ae2da60dcce
Tri-Quyet <156794243+quyetnt88@users.noreply.github.com>
2025-06-21 05:27:32 +0700

    Dev (#58)
    * add feedback and feedback type api  (#53)
    
    * add create and get feedback type
    
    * add create and get feedback api
    
    * add update, delete and get by id API for feedback type
    
    * add http client service
    
    * Trungn (#54)
    
    * modify auth endpoints
    
    * fix auth request dto
    
    * add momo payment service
    
    * add http processor (#55)
    
    * add create and get feedback type
    
    * add create and get feedback api
    
    * add update, delete and get by id API for feedback type
    
    * add http client service
    
    * add http context
    
    * add http context
    
    * fix conflict
    
    * complete createMomoPayment Api
    
    * fix type
    
    ---------
    
    Co-authored-by: Le Hoang Khanh <150586464+khanhhoang209@users.noreply.github.com>
    Co-authored-by: Nguyen Thanh Trung <trungnguyen0803forwork@gmail.com>
    Co-authored-by: buigiahuy403 <buigiahuy403@gmail.com>

---
+++ OrderService/src/Application/MomoPayment/OptionModel/MomoOptionModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderService.Application.OptionModel;
+
+public class MomoOptionModel
+{
+
+    [Required] public string? PartnerCode { get; set; }
+
+    [Required] public string? AccessKey { get; set; } 
+
+    [Required] public string? SecretKey { get; set; } 
+
+    [Required] public string? PaymentUrl { get; set; }
+    
+    [Required] public string? RedirectUrl { get; set; }
+
+    [Required] public string? IpnUrl { get; set; }
+
+    [Required] public string? RequestType { get; set; }    
+
+}
---
+++ OrderService/src/Web/Endpoints/Payment.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using OrderService.Application.MomoPayment.Commands.CreateMomoPayment;
+
+namespace OrderService.Web.Endpoints;
+
+
+public class Payment : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .DisableAntiforgery()
+            .MapPost(MomoPayment, "/momo/create");
+    }
+
+    public static async Task<IResult> MomoPayment([FromBody] CreateMomoPaymentCommand command,
+        ISender sender
+        )
+    {
+        var response = await sender.Send(command);
+
+        if (response != null)
+        {
+            return TypedResults.Ok(response);
+        }
+        else return TypedResults.BadRequest();
+    }
+}
